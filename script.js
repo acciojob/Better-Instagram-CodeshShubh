6c67d660e24fd9996c0c3bae28741ed4af7739da
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-15 15:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+ const images = document.querySelectorAll(".image");
+let draggedImage = null;
+let targetImage = null;
+
+// Add drag-and-drop event listeners to each image
+images.forEach((image) => {
+  image.addEventListener("dragstart", (e) => {
+    draggedImage = e.target; 
+  }); 
+
+  // When dragging ends
+  image.addEventListener("dragend", (e) => {
+    draggedImage = null; // Clear the reference
+	targetImage = null;
+  });
+
+  // Allow drop by preventing default behavior
+  image.addEventListener("dragover", (e) => {
+    e.preventDefault();
+	  targetImage = e.target; // Store the image being dragged over
+  });
+
+  // Handle the drop
+  image.addEventListener("drop", () => {
+    if(draggedImage && targetImage){
+		const temHTML = draggedImage.innerHTML;
+		draggedImage.innerHTML = targetImage.innerHTML;
+		targetImage.innerHTML = temHTML;
+	}
+  });
+});
+
